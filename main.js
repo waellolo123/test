910d59c81b0f726676e16bb01c9ea1c7d9ee4ea7
wael latrech <wael.22@live.fr>
2023-05-19 20:16:39 +0200

    some changes

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
 const navBtn = document.querySelector('.humburger')
 const menu = document.querySelector('.navbar1-list')
+const cards = document.querySelectorAll('.card')
+
+// nav slide
 navBtn.addEventListener('click', () => {
     menu.classList.toggle('active')
 })
 // Scroll Animation
-const cards = document.querySelectorAll('.card')
-window.addEventListener('scroll', cards)
+let options = {
+    rootMargin: "-100px 0px",
+    threshold: 0
+}
+function handleIntersect(entries){
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.style.opacity = 1;
+            entry.target.style.transform = "translateY(0px)"
+        }
+    })
+}
+const observer = new IntersectionObserver
+(handleIntersect, options)
 
+cards.forEach(card => {
+    observer.observe(card)
+})
