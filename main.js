95580855c71f5664fbae64bf174b04bf7a46778d
wael latrech <wael.22@live.fr>
2023-05-19 19:52:52 +0200

    some changes

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const navBtn = document.querySelector('.humburger')
 const menu = document.querySelector('.navbar1-list')
-
 navBtn.addEventListener('click', () => {
     menu.classList.toggle('active')
 })
+// Scroll Animation
+const cards = document.querySelectorAll('.card')
+window.addEventListener('scroll', cards)
+
